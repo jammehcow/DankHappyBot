dc0f44802ad90db67728b347527c557b6912c855
James Upjohn <jammehcow@jammehcow.co.nz>
2021-04-30 18:40:20 +1200

    Add basic IRC functionality to service

--- src/DankHappyBot.Bot/IrcHostedService.cs
+++ src/DankHappyBot.Bot/IrcHostedService.cs
@@ -1,6 +1,8 @@
 using System.Threading;
 using System.Threading.Tasks;
 using DankHappyBot.Service.Configuration;
+using GravyIrc;
+using GravyIrc.Connection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -12,6 +14,8 @@ namespace DankHappyBot.Bot
 
         private readonly IrcOptions _ircOptions;
 
+        private IrcClient IrcClient { get; set; }
+
         public IrcHostedService(ILogger<IrcHostedService> logger, IrcOptions ircOptions)
         {
             _logger = logger;
@@ -20,7 +24,25 @@ namespace DankHappyBot.Bot
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            // stub
+            _logger.LogInformation("Starting IRC client with username {Username}", _ircOptions.Credentials.Username);
+
+            var ircUser = new User(_ircOptions.Credentials.Username);
+
+            // TODO: move TcpClientConnection to factory
+            IrcClient = new IrcClient(ircUser, _ircOptions.Credentials.OAuthToken, new TcpClientConnection());
+
+            await IrcClient.ConnectAsync(_ircOptions.Connection.Address, _ircOptions.Connection.Port);
+
+            // Wait until cancelled
+            // TODO: allow for managed cancel instead of relying on console kill
+            await Task.Delay(-1, stoppingToken);
+
+            _logger.LogInformation("Stopping IRC client");
+        }
+
+        public override void Dispose()
+        {
+            IrcClient?.Dispose();
         }
     }
 }
